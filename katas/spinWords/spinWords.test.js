ae6b2d63e2d9c987c0003ad1a5917989016e2351
Jared Headings <jaredmheadings@gmail.com>
2025-03-03 12:07:23 -0500

    Refactored this test slightly to use the same pattern used in tribonacci (putting the it() function call inside a doTest, so you can see test cases)

--- katas/spinWords/spinWords.test.js
+++ katas/spinWords/spinWords.test.js
@@ -1,14 +1,18 @@
 import spinWords from "./spinWords.js";
-import { assert } from "chai"
+import { assert } from "chai";
 
-describe("Spinning words",()=>{
-    it("Sample tests",()=>{
-      assert.strictEqual(spinWords("Welcome"), "emocleW");
-      assert.strictEqual(spinWords("Hey fellow warriors"), "Hey wollef sroirraw");
-      assert.strictEqual(spinWords("This is a test"), "This is a test");
-      assert.strictEqual(spinWords("This is another test"), "This is rehtona test");
-      assert.strictEqual(spinWords("You are almost to the last test"), "You are tsomla to the last test");
-      assert.strictEqual(spinWords("Just kidding there is still one more"), "Just gniddik ereht is llits one more");
-      assert.strictEqual(spinWords("Seriously this is the last one"), "ylsuoireS this is the last one");
-    });
+function doTest(input, expected) {
+  it(`Should spin "${input}" to "${expected}"`, () => {
+    assert.strictEqual(spinWords(input), expected);
   });
+}
+
+describe("Spinning words", () => {
+  doTest("Welcome", "emocleW");
+  doTest("Hey fellow warriors", "Hey wollef sroirraw");
+  doTest("This is a test", "This is a test");
+  doTest("This is another test", "This is rehtona test");
+  doTest("You are almost to the last test", "You are tsomla to the last test");
+  doTest("Just kidding there is still one more", "Just gniddik ereht is llits one more");
+  doTest("Seriously this is the last one", "ylsuoireS this is the last one");
+});
